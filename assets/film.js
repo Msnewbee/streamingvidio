b1fe868ec358d86ae94033484ff65a924aee4edc
Msnewbee <bilariko2@gmail.com>
2025-03-22 03:53:52 +0700

    Update film.js

--- assets/film.js
+++ assets/film.js
@@ -35,12 +35,20 @@ function verifyAge() {
 function loadFilms() {
     const films = [
         {
-            title: "Film 1",
+            title: "Langkah kaka membantu adik nya cum",
             embed: "https://terabox.com/sharing/embed?surl=oEY8jxE4Tv0pJaz6d_2oYQ&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=843976327435458&slid="
         },
         {
-            title: "Film 2",
+            title: "Cewe cantik Ini beramain sendirian di kamar mandi",
             embed: "https://terabox.com/sharing/embed?surl=8eNwlybnhiOoLzAC__1MGw&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=1017568112374262&slid="
+        }
+        {
+            title: "Dinda bermain dengan bantal",
+            embed: "https://terabox.com/sharing/embed?surl=355FHPJlwRhisnF8keIbjg&resolution=360&autoplay=true&mute=false&uk=81366251617206&fid=65017207422990&slid="
+        },
+        {
+            title: "mengintip kaka nya bermain di kamar mandi",
+            embed: "https://terabox.com/sharing/embed?surl=OcKoQEy8pG5vTiLMr2WCMg&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=732675653952914&slid="
         }
     ];
 
