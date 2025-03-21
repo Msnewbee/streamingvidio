27e6029a31e8923b6e4cb79ba3b514dd3c1cb74d
Msnewbee <bilariko2@gmail.com>
2025-03-21 22:02:20 +0700

    Update film.js

--- assets/film.js
+++ assets/film.js
@@ -1,15 +1,4 @@
-const films = [
-    {
-        title: "Film Dewasa 1",
-        embed: "https://terabox.com/sharing/embed?surl=oEY8jxE4Tv0pJaz6d_2oYQ&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=843976327435458&slid="
-    },
-    {
-        title: "Film Dewasa 2",
-        embed: "https://terabox.com/sharing/embed?surl=EXAMPLELINK"
-    }
-];
-
-function verifyAge() {
+window.verifyAge = function () {
     const dob = document.getElementById('dob').value;
     if (!dob) {
         alert('Silakan masukkan tanggal lahir Anda.');
@@ -31,11 +20,22 @@ function verifyAge() {
         loadFilms();
     } else {
         alert('Maaf, Anda harus berusia minimal 18 tahun untuk mengakses konten ini.');
-        window.location.href = 'index.html'; // Alihkan ke halaman utama jika di bawah 18 tahun
+        window.location.href = 'index.html';
     }
-}
+};
 
-function loadFilms() {
+window.loadFilms = function () {
+    const films = [
+        {
+            title: "Film Dewasa 1",
+            embed: "https://terabox.com/sharing/embed?surl=oEY8jxE4Tv0pJaz6d_2oYQ&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=843976327435458&slid="
+        },
+        {
+            title: "Film Dewasa 2",
+            embed: "https://terabox.com/sharing/embed?surl=EXAMPLELINK"
+        }
+    ];
+
     const filmList = document.getElementById('film-list');
     films.forEach(film => {
         const filmContainer = document.createElement('div');
@@ -46,4 +46,4 @@ function loadFilms() {
         `;
         filmList.appendChild(filmContainer);
     });
-}
+};
