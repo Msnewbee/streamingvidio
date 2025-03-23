65cf8c5a917a38bd3f90cdc1c9efea9d41b5b296
Msnewbee <bilariko2@gmail.com>
2025-03-24 00:47:42 +0700

    Update film.js

--- assets/film.js
+++ assets/film.js
@@ -33,16 +33,20 @@ function verifyAge() {
 function loadFilms() {
     const films = [
         {
-            title: "Film Dewasa 1",
-            embed: "https://trusted-video-host.com/embed/video1"
+            title: "Langkah kaka membantu adik nya cum",
+            embed: "https://terabox.com/sharing/embed?surl=oEY8jxE4Tv0pJaz6d_2oYQ&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=843976327435458&slid="
         },
         {
-            title: "Film Dewasa 2",
-            embed: "https://trusted-video-host.com/embed/video2"
+            title: "mengintip kaka nya bermain di kamar mandi",
+            embed: "https://terabox.com/sharing/embed?surl=8eNwlybnhiOoLzAC__1MGw&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=1017568112374262&slid="
         },
         {
-            title: "Film Dewasa 3",
-            embed: "https://trusted-video-host.com/embed/video3"
+            title: "Dinda bermain dengan bantal",
+            embed: "https://terabox.com/sharing/embed?surl=355FHPJlwRhisnF8keIbjg&resolution=360&autoplay=true&mute=false&uk=81366251617206&fid=65017207422990&slid="
+        },
+        {
+            title: "Ibu rere bermain dengan botol besar",
+            embed: "https://terabox.com/sharing/embed?surl=OcKoQEy8pG5vTiLMr2WCMg&resolution=1080&autoplay=true&mute=false&uk=81366251617206&fid=732675653952914&slid="
         }
     ];
 
